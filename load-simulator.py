9c0f73e97f567c1737eb31d4bb201d9887e602c2
CarlosDanielSLima <carlitosdellima@gmail.com>
2026-04-05 02:05:42 -0300

    Update: Definicao do formato do ritmo de execucao e importacao de bibliotecas para a criacao assincrona das requisicoes.

--- load-simulator.py
+++ load-simulator.py
@@ -1,22 +1,46 @@
+import asyncio
+import httpx
+import time
+import statistics
+import random
+
+
 # Define variaveis
-N_CLIENTES      = 1000
-N_RESTAURANTES  = 50
-N_ENTREGADORES  = 3 * N_CLIENTES
-N_MILISSEGUNDOS = 60 * 3
-N_DDB_ITEMS     = 50000
+N_CLIENTES     = 1000
+N_RESTAURANTES = 50
+N_ENTREGADORES = 3 * N_CLIENTES
+N_DDB_ITEMS    = 50000
 VOLUMES = {
     "OPERACAO_NORMAL": 10,
     "PICO": 50,
     "EVENTO_ESPECIAL": 200
 }
+RITMO_EXEC = [
+    {
+        "volume": "OPERACAO_NORMAL",
+        "duracao": 60
+    },
+    {
+        "volume": "PICO",
+        "duracao": 20
+    },
+    {
+        "volume": "EVENTO_ESPECIAL",
+        "DURACAO": 40
+    },
+    {
+        "volume": "OPERACAO_NORMAL",
+        "DURACAO": 20
+    },
+    {
+        "volume": "PICO",
+        "DURACAO": 40
+    }
+]
 
 
 # Popular o sistema com dados basicos
 
 
-# Executar as operacoes por segundo
-# Entregador reporta posicao (1000 ms)
 # Cliente pode consultar o status do pedido (estado, entregador e posicao)
-# Adm pode consultar histórico de pedidos de um cliente (Ordem cronologica)
-# Adm pode consultar histórico de eventos de um pedido (Ordem cronologica)
 # API deve responder em menos de 500ms no 95 percentil
