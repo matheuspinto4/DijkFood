bdc97daa5ec728acd4ca930d59c8847c145faf19
CarlosDanielSLima <carlitosdellima@gmail.com>
2026-04-05 02:24:28 -0300

    Add: Adiciona a funcao main definindo o esqueleto do simulador, que ira executar cada um dos volumes definidos no ritmo de execucao. Essa abordagem permite testar diversos cenarios em sequencia.

--- load-simulator.py
+++ load-simulator.py
@@ -10,6 +10,9 @@ N_CLIENTES     = 1000
 N_RESTAURANTES = 50
 N_ENTREGADORES = 3 * N_CLIENTES
 N_DDB_ITEMS    = 50000
+
+URL = "http://localhost:8000"
+CONCURRENCY = 40 # Delay de 0.5s da api + max volume 200, essa capacidade da conta de criar aos pedidos no segundo em que foram planejados.
 VOLUMES = {
     "OPERACAO_NORMAL": 10,
     "PICO": 50,
@@ -44,3 +47,35 @@ RITMO_EXEC = [
 
 # Cliente pode consultar o status do pedido (estado, entregador e posicao)
 # API deve responder em menos de 500ms no 95 percentil
+orders = []
+orders_lock = asyncio.Lock()
+
+async def main():
+    queue = asyncio.Queue()
+    results = []
+
+    # Cria requesters para realizarem as requisicoes a api
+    requesters = [
+        asyncio.create_task(requester(queue, results))
+        for _ in range(CONCURRENCY)
+    ]
+
+    for ritmo in RITMO_EXEC:
+        producers = [
+            asyncio.create_task(producer_order(queue, ritmo["volume"], ritmo["duracao"])),
+            asyncio.create_task(view_order(queue, ritmo["duracao"])),
+        ]
+
+        start_ritmo = time.perf_counter()
+
+        await asyncio.gather(*producers)
+
+        await queue.join()
+
+        ritmo_time = time.perf_counter() - start_ritmo
+
+    # Finaliza requesters
+    for _ in requesters:
+        await queue.put(None)
+
+    await asyncio.gather(*requesters)
