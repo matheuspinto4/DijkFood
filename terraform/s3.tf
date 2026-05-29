fa20695bd64f240294425d437b68eae85232cf5e
matheuspinto4 <mat.vsp@gmail.com>
2026-05-29 12:24:09 -0300

    Correção: erro de digitacao

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "grafo" {
     bucket = "dijkfood-grafo-sp-${data.aws_caller_identity.current.account_id}"
+    force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "grafo" {
